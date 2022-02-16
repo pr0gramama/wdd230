36033552889ce89d8b42a3b4868baca009189b6a
pr0gramama <paleofemme@gmail.com>
2022-02-16 15:09:36 -0700

    Week 7 initial commit

---
+++ lesson-7/script.js
@@ -0,0 +1,37 @@
+// get all imgs with data-src attribute
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+// optional parameters being set for the IntersectionObserver
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+   image.setAttribute("src", image.getAttribute("data-src"));
+   image.onLoad = () => {
+       image.removeAttribute("data-src")
+    ;};
+};
+
+// first check to see if IntersectionObserver is supported
+if ("IntersectionObserver" in window) {
+    const  observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    })
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
