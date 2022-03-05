3aa7c5234edbded90db9cd64e69c1b5c400fd801
pr0gramama <paleofemme@gmail.com>
2022-03-04 17:02:52 -0700

    js and css fixes

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,59 +1,126 @@
-document.querySelector('.grid-button').addEventListener("click", () => {
-    document.querySelector(".cards").classList.add("grid-layout");
-    document.querySelector(".cards").classList.add("list-layout");
-}, false);
 
-document.querySelector('.list-button').addEventListener("click", () => {
-    document.querySelector(".cards").classList.add("list-layout");
-    document.querySelector(".cards").classList.add("grid-layout");
-}, false);
 
 const requestURL = "https://pr0gramama.github.io/wdd230/chamber/data/data.json"
 
+const cards = document.querySelector(".member-cards");
+const memlist = document.querySelector(".member-list");
+
 fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-        const members = jsonObject["members"];
-        console.table(jsonObject);
-        members.forEach(displayMembers);
+.then(function (response) {
+    return response.json();
+})
+.then(function (jsonObject) {
+    console.table(jsonObject);
+    let members = jsonObject["members"];
+    members.forEach(displayMembers);
+
+    document.getElementById("grid-button").addEventListener("click", () => {
+        members.forEach(displayMembersList)
+        cards.innerHTML = ""
     });
+    
+    document.getElementById("list-button").addEventListener("click", () => {
+        members.forEach(displayMembers)
+        memlist.innerHTML = ""
+        
+    });
+})
 
 function displayMembers(member) {
     let card = document.createElement('section');
     
     //h2 name
-    let h2 = document.createElement('h2');
-    h2.textContent = `${member.name}`;
-    card.appendChild(h2);
+    let name = document.createElement('h2');
+    name.textContent = `${member.name}`;
+    card.appendChild(name);
     
-    //p birth date
-    let p1 = document.createElement('p');
-    p1.textContent = `${member.address}`;
-    card.appendChild(p1);
+    //p address
+    let address = document.createElement('p');
+    address.textContent = `${member.address}`;
+    card.appendChild(address);
     
-    //p birth place
-    let p2 = document.createElement('p');
-    p2.textContent = `${member.phone}`;
-    card.appendChild(p2);
+    //p phone
+    let phone = document.createElement('p');
+    phone.textContent = `${member.phone}`;
+    card.appendChild(phone);
     
     //website
-    let p3 = document.createElement("p");
-    p3.textContent = `${member.website}`;
-    card.appendChild(p3);
+    let website = document.createElement("p");
+    website.textContent = `${member.website}`;
+    card.appendChild(website);
 
-    
     // img
-    
     let logo = document.createElement('img');
-    logo.classList.add('member-logo');
     logo.setAttribute("src", member.logo);
     logo.setAttribute("alt", `${member.name} Logo`);
     logo.setAttribute("loading", "lazy");
     card.appendChild(logo);
     
-    
-    document.querySelector('div.cards').appendChild(card);
+    cards.appendChild(card);
 }
 
+function removedisplayMembers () {
+    let card = document.createElement("section");
+    cards.prepend(card);
+}
+
+function displayMembersList(memberList) {
+    let mlist = document.createElement("table");
+
+    //td name
+    let name = document.createElement("td");
+    name.textContent = `${memberList.name}`;
+    mlist.appendChild(name);
+
+    //td address
+    let address = document.createElement("td");
+    address.textContent = `${memberList.address}`;
+    mlist.appendChild(address);
+
+    //td phone
+    let phone = document.createElement("td");
+    phone.textContent = `${memberList.phone}`;
+    mlist.appendChild(phone);
+
+    //td website
+    let website = document.createElement("td");
+    website.textContent = `${memberList.website}`;
+    mlist.appendChild(website);
+
+    memlist.appendChild(mlist);
+}
+
+// function displayMembers(member) {
+//     let card = document.createElement('section');
+    
+//     //h2 name
+//     let h2 = document.createElement('h2');
+//     h2.textContent = `${member.name}`;
+//     card.appendChild(h2);
+    
+//     //p birth date
+//     let p1 = document.createElement('p');
+//     p1.textContent = `${member.address}`;
+//     card.appendChild(p1);
+    
+//     //p birth place
+//     let p2 = document.createElement('p');
+//     p2.textContent = `${member.phone}`;
+//     card.appendChild(p2);
+    
+//     //website
+//     let p3 = document.createElement("p");
+//     p3.textContent = `${member.website}`;
+//     card.appendChild(p3);
+    
+//     // img
+//     let logo = document.createElement('img');
+//     logo.classList.add('member-logo');
+//     logo.setAttribute("src", member.logo);
+//     logo.setAttribute("alt", `${member.name} Logo`);
+//     logo.setAttribute("loading", "lazy");
+//     card.appendChild(logo);
+    
+    
+//     document.querySelector('div.cards').appendChild(card);
+// }
