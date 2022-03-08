237283af432cf2fb9eb6f3e6cec968be8a9f7fb0
pr0gramama <paleofemme@gmail.com>
2022-03-08 13:00:04 -0700

    weather

---
+++ js/weatherapi.js
@@ -0,0 +1,15 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=7024811e8910a63973752b5b2868b3a1";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject)
+    document.querySelector(`#current-temp`).textContent = jsObject.main.temp;
+
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+    });
