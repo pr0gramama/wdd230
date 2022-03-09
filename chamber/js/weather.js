90f0d42422e36f1dc92eca5c556af4ef24a658a0
pr0gramama <paleofemme@gmail.com>
2022-03-09 10:11:27 -0700

    weather widget

--- chamber/js/weather.js
+++ chamber/js/weather.js
@@ -1,14 +1,16 @@
+// Pinnacle City is a made up place, so I am using data from Farmington, UT; city id 5774662
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5774662&units=imperial&appid=7024811e8910a63973752b5b2868b3a1";
 
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         console.log(jsObject)
-    document.querySelector(`#current-temp`).textContent = jsObject.main.temp;
+    document.querySelector("#current-temp").textContent = jsObject.main.temp;
+    
 
     const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-    const desc = jsObject.weather[0].description;
-    document.querySelector('#icon-src').textContent = iconsrc;
+    const desc = jsObject.weather[0].description; 
+    // document.querySelector('#icon-src').textContent = iconsrc;
     document.querySelector('#weathericon').setAttribute('src', iconsrc);
     document.querySelector('#weathericon').setAttribute('alt', desc);
     document.querySelector('figcaption').textContent = desc;
@@ -16,4 +18,4 @@ fetch(apiURL)
     document.querySelector('#wind').textContent = jsObject.wind.speed;
     });
 
-  //farmington id 5774662
+    
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,11 +1,11 @@
 
 
-const t = parseInt(document.querySelector("#currentTemp").textContent);
-const w = parseInt(document.getElementById("wind").textContent);
+const t = parseInt(document.querySelector("#current-temp").textContent);
+const w = parseInt(document.querySelector("#wind").textContent);
 
 if(t <= 50 && w > 3) {
-    const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(w,0.16) + 0.4275 * t * Math.pow(w,0.16)
-    document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}`;
+    const windChill = 35.74 + 0.6215 * t - 35.75 * Math.pow(w,0.16) + 0.4275 * t * Math.pow(w,0.16)
+    document.querySelector("#windchill").innerHTML = `${Math.round(windChill)}`;
 }
 else {
     document.querySelector("#windchill").innerHTML = "N/A"
